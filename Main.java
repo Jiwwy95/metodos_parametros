6d6e749270d9c4b87b21e0db0203817126440d5a
Jiwwy95 <jjcastillo03@tecnologicosudamericano.edu.ec>
2024-06-05 09:58:21 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,58 +1,21 @@
 package org.example;
-
-import java.util.Random;
 import java.util.Scanner;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
-    //Metodo con multiples parametros
-
-    public static void calculate(int a, int b) {
-        int sum=a+b;
-        int product=a*b;
-        System.out.println("La suma es: "+sum);
-        System.out.println("El producto es: "+product);
-    }
-
-    //Metodo recursivo para calcular el factorial de uin numero
-
-    public static int factorial(int n){
-        if (n==0){
-            return 1;
-        } else {
-            return n*factorial(n-1);
-        }
-    }
-
-    //Generacion de numeros aleatorios
-
-    public static int generateRandomNumber(int min, int max){
-        Random random = new Random();
-        return random.nextInt((max - min) + 1) + min;
-    }
-
-    //Juego de probabilidad
-
-    public static void playGame(){
-        Scanner scanner = new Scanner(System.in);
-        int randomNumber = generateRandomNumber(1, 100);
-        System.out.println("Adivina el numero entre 1 y 100");
-        int guess = scanner.nextInt();
-        if (guess == randomNumber){
-            System.out.println("Correcto !Adivinaste el numero");
-        } else {
-            System.out.println("Incorrecto !El numero era: "+ randomNumber);
-        }
-    }
-
     public static void main(String[] args) {
-        //Uso de metodos con multiples parametros
-        calculate(3,7);
-        // Uso de metodo recursivo
-        int num=5;
-        System.out.println("El factorial de "+num+" es: "+factorial(num));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresar la palabra o frase a invertir: ");
+        String texto = scanner.nextLine();
+        System.out.println("La palabra o frase que ingresada es: " + texto);
+        String invertir = invertirCadena(texto);
+        System.out.println("Texto ingresado invertido: " + invertir);
+        scanner.close();
 
-        //Juego de probabilidad
-        playGame();
+    }
+
+    public static String invertirCadena(String texto) {
+        StringBuilder builder = new StringBuilder(texto);
+        return builder.reverse().toString();
     }
 }
